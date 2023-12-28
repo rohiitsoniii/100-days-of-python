a8f2164ad3375b08135f6f8d52fe592efd5debd5
Rohit Soni <rohitsoniahsi001@gmail.com>
2023-12-29 00:08:10 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("welcome to band name genrator")
+city = input("Which city did you grew up? ")
+pet = input("what is the name of your pet? ")
+print("your band name could be " +city +" " + pet)
