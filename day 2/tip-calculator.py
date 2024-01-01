93143140210dd189c81f0a91cc222bbe78d178c8
Rohit Soni <rohitsoniahsi001@gmail.com>
2024-01-01 22:43:50 +0530

    chnges

--- day 2/tip-calculator.py
+++
@@ -1,6 +0,0 @@
-print("welcome to tip calculator")
-total_bill = int(input("what is the total bill? "))
-tip_per = int(input("what percentage tip you would like to give ? 10,12 or 15? "))
-num_people = int(input("How many people to split the bill? "))
-result = round((total_bill*((100+tip_per)/100))/num_people)
-print(result)
--- day1.py
+++
@@ -1,4 +0,0 @@
-print("welcome to band name genrator")
-city = input("Which city did you grew up? ")
-pet = input("what is the name of your pet? ")
-print("your band name could be " +city +" " + pet)
